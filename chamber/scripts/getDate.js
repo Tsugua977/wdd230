f2f270234396e4162380d9a2fdd6bf1a513a7421
Tsugua977 <augustpelissero@gmail.com>
2022-10-07 19:26:47 -0700

    Finished shrinking css.

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -1,3 +1,5 @@
+const dateDiv = document.querySelector(".fullDate");
+
 let d = new Date();
 let getYear = d.getFullYear();
 let lastMod = new Date(document.lastModified);
@@ -5,5 +7,5 @@ let date = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(d);
 
 document.getElementById("year").innerHTML = getYear;
 document.getElementById("lastUpdate").innerHTML = lastMod;
-document.querySelector(".fulldate").innerHTML = date;
 
+dateDiv.innerHTML = date;
