c2c7a8adeb086f1d0bfcf9adfda457a7e94b144b
Tsugua977 <augustpelissero@gmail.com>
2022-10-25 16:18:29 -0700

    Worked on image loading

---
+++ lesson 7/scripts/lazyLoad.js
@@ -0,0 +1,30 @@
+const images = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onLoad = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+images.forEach((img) => {
+    loadImages(img);
+});
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    
+    images.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    images.forEach((img) => {
+        loadImages(img);
+    });
+}
