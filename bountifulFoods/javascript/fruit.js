7866e6d1ff46b0677646f8ebca4db2ccd583eefe
Tsugua977 <augustpelissero@gmail.com>
2022-12-12 11:10:21 -0700

    Fixed fruits.json display

--- bountifulFoods/javascript/fruit.js
+++ bountifulFoods/javascript/fruit.js
@@ -7,30 +7,40 @@ fetch(requestURL)
         return response.json();
     })
     .then(function(jsonObject){
-        console.log(jsonObject);
         const fruitsData = jsonObject;
         console.log("fruistdata:",fruitsData);
         fruitsData.forEach(displayFruits);
             
 });
 
-function example(element) {
-    console.log(element);
+const select1 = document.querySelector('#fruitchoice1');
+const output = select1.value;
+console.log(output);
+
+function displayFruits(fruits ) {
+    let choice = document.createElement('option');
+
+    choice.setAttribute('value', fruits.name);
+    choice.text = fruits.name;
+
+    console.log("fruit", choice);
+    
+    const choice1 = document.getElementById('fruitchoice1');
+    choice1.appendChild(choice);
+    
+    /*let dropdown = document.createElement('select');
+    let label = document.createElement('label');
+    let choice = document.createElement('option');
+    dropdown.setAttribute('name', 'firstFruit');
+    dropdown.setAttribute('id', 'fruitchoice' + incrementsFruit++);
+    choice.setAttribute('value', fruits.name);
+    choice.innerHTML = fruits.name;
+    
+    label.setAttribute('class', 'fruitLabel');
+    label.textContent = "Choose a fruit:";
+    label.appendChild(dropdown)
+    dropdown.appendChild(choice)
+    
+    document.querySelector('.fruitSelect').appendChild(label);*/
 }
 
-function displayFruits(fruits) {
-    let checkbox = document.createElement('input');
-    let label = document.createElement('label');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.setAttribute('class', 'fruitCheck');
-    checkbox.setAttribute('name', 'typeOfFruit' + incrementsFruit++);
-    checkbox.setAttribute('value', fruits.name);
-    
-    label.setAttribute('class', 'fruitLabel' + incrementsLabel++);
-    label.textContent = fruits.name;
-    label.appendChild(checkbox)
-    
-    document.querySelector('.fruitSelect').appendChild(label);
-    
-    
-}
